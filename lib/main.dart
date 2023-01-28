bd384fa661e7124a5a97d473dc6d5c66363138b9
Ahmedkhalil98 <ahmedkhlil756@gmail.com>
2023-01-28 09:25:04 -0800

    create project

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:home_service/root.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/root.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:home_service/Screens/Home_Screen/home_Page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomePage(),
+        );
+      },
+    );
+  }
+}
